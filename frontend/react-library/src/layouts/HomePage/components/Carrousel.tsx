5651beca759a0b9ef0163301d840f6a3d2e12ab0
Diogo Almeida <diogodma80@gmail.com>
2023-07-25 09:57:29 -0400

    Call Spring Boot API from React with useEffect

--- frontend/react-library/src/layouts/HomePage/components/Carrousel.tsx
+++ frontend/react-library/src/layouts/HomePage/components/Carrousel.tsx
@@ -1,67 +1,117 @@
+import BookModel from "../../../models/BookModel";
 import { ReturnBook } from "./ReturnBook";
+import { useEffect, useState } from "react";
 
 export const Carrousel = () => {
+
+    const [books, setBooks] = useState<BookModel[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const baseUrl: string = "http://localhost:8080/api/books"
+            const url:string = `${baseUrl}?page=0&size=9`;
+
+            //fetches data from the api and puts it in response
+            const response = await fetch(url);
+
+            // if there is no response data
+            if(!response.ok) {
+                throw new Error("Something went wrong!")
+            }
+
+            //turns response into json
+            const responseJson = await response.json();
+
+            const responseData = responseJson._embedded.books;
+
+            const loadedBooks: BookModel[] = [];
+
+            for(const key in responseData) {
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].author,
+                    description: responseData[key].description,
+                    copies: responseData[key].copies,
+                    copiesAvailable: responseData[key].copiesAvailable,
+                    category: responseData[key].category,
+                    img: responseData[key].img
+                });
+            }
+
+            setBooks(loadedBooks);
+            setIsLoading(false);
+        };
+
+        fetchBooks().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+    }, []);
+
     return (
-<div className="container mt-5" style={{height: 550}}>
+        <div className="container mt-5" style={{height: 550}}>
 
-    <div className="homepage-carousel-title">
-        <h3>
-            Find your next "I stayed up too late reading" book.
-        </h3>
-    </div>
-
-    <div id="carouselExampleControls" className="carousel carousel-dark slide mt-5 d-none d-lg-block" data-bs-interval='false'>
-
-        {/* Desktop */}
-        <div className="carousel-inner">
-            <div className="carousel-item active">
-                <div className="row d-flex justify-content-center align-items-center">
-                    <ReturnBook />
-                    <ReturnBook />
-                    <ReturnBook />
-                </div>
+            <div className="homepage-carousel-title">
+                <h3>
+                    Find your next "I stayed up too late reading" book.
+                </h3>
             </div>
 
-            <div className="carousel-item">
-                <div className="row d-flex justify-content-center align-items-center">
-                    <ReturnBook />
-                    <ReturnBook />
-                    <ReturnBook />
-                </div>
-            </div>
+            <div id="carouselExampleControls" className="carousel carousel-dark slide mt-5 d-none d-lg-block" data-bs-interval='false'>
 
-            <div className="carousel-item">
-                <div className="row d-flex justify-content-center align-items-center">
-                    <ReturnBook />
-                    <ReturnBook />
-                    <ReturnBook />
+                {/* Desktop */}
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <div className="row d-flex justify-content-center align-items-center">
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
+                        </div>
+                    </div>
+
+                    <div className="carousel-item">
+                        <div className="row d-flex justify-content-center align-items-center">
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
+                        </div>
+                    </div>
+
+                    <div className="carousel-item">
+                        <div className="row d-flex justify-content-center align-items-center">
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
+                        </div>
+                    </div>
                 </div>
+                <button className="carousel-control-prev" type="button" data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
+                    <span className="carousel-control-prev-icon" aria-hidden='true'></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                
+                <button className="carousel-control-next" type="button" data-bs-target='#carouselExampleControls' data-bs-slide='next'>
+                    <span className="carousel-control-next-icon" aria-hidden='true'></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
-        </div>
-        <button className="carousel-control-prev" type="button" data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
-            <span className="carousel-control-prev-icon" aria-hidden='true'></span>
-            <span className="visually-hidden">Previous</span>
-        </button>
-        
-        <button className="carousel-control-next" type="button" data-bs-target='#carouselExampleControls' data-bs-slide='next'>
-            <span className="carousel-control-next-icon" aria-hidden='true'></span>
-            <span className="visually-hidden">Next</span>
-        </button>
-    </div>
     
 
-    {/* Mobile */}
-    <div className="d-lg-none mt-3">
-        <div className="row d-flex justify-content-center align-items-center">
-            <ReturnBook />
-        </div>
-    </div>
+            {/* Mobile */}
+            <div className="d-lg-none mt-3">
+                <div className="row d-flex justify-content-center align-items-center">
+                    <ReturnBook />
+                </div>
+            </div>
 
-    <div className="homepage-carousel-title mt-3">
-        <a href="#" className="btn btn-outline-secondary btn-lg">
-            View More
-        </a>
-    </div>
-</div>
+            <div className="homepage-carousel-title mt-3">
+                <a href="#" className="btn btn-outline-secondary btn-lg">
+                    View More
+                </a>
+            </div>
+        </div>
     );
 }
